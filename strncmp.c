93686584f7374468df72e9637dfd1aa3be81aecf
Federico Damian Estebanez <fdamian-@c1r9s4.42malaga.com>
2023-09-23 21:36:02 +0200

    strrchar

---
+++ strncmp.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   strncmp.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdamian- < fdamian-@student.42malaga.co    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/23 21:23:26 by fdamian-          #+#    #+#             */
+/*   Updated: 2023/09/23 21:35:47 by fdamian-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			cnt;
+	unsigned int	diff;
+
+	cnt = 0;
+	diff = 0;
+	while ((cnt < n) && !diff && (s1[cnt] != 0) && (s2[cnt] != 0))
+	{
+		diff = (unsigned char)s1[cnt] - (unsigned char)s2[cnt];
+		cnt++;
+	}
+	if (cnt < n && !diff && (s1[cnt] == 0 || s2[cnt] == 0))
+		diff = (unsigned char)s1[cnt] - (unsigned char)s2[cnt];
+	return (diff);
+}
